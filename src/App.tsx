fb7ff323751ab57f1d9ca4b8b5b87c3dcb0f4b7e
junghochoi <dchoi315@gmail.com>
2021-10-17 15:48:09 -0400

    login-page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import Login from './LoginPage/Login';
+
+import './App.css';
 
 const App = () => {
     return (
-        <h1>Hello World!</h1>
+
+
+        <Login />
     );
 }
 
---
+++ src/LoginPage/Login.tsx
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import './Login.css'
+function Login() {
+  return (
+    <div className='login-page'>
+    
+      <div className='login-page-background'>
+
+      </div>
+    </div>
+  )
+}
+
+export default Login
