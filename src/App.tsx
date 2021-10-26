3f7ece842b904178d8ae021273e6a6e2ac6aa6a9
Stanley Thomas <stanleyt4405@gmail.com>
2021-10-26 19:40:37 -0400

    added firebase config

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,8 @@
 import React from 'react';
 import firebase from './config/firebase';
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 const App = () => {
     const handleClick = () => {
-        const auth = getAuth();
-        createUserWithEmailAndPassword(auth, "stanleyt4405@gmail.com", "password123")
-        .then((userCredential) => {
-            // Signed in 
-            const user = userCredential.user;
-            console.log(JSON.stringify(user))
-            // ...
-        })
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            console.log(errorMessage);
-        });
-        
     }
     return (
         <div>
