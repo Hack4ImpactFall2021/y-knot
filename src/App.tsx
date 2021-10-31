0130210a713060f4abe5e605cdfaf05c2c8e44ed
junghochoi <dchoi315@gmail.com>
2021-10-31 00:57:06 -0400

    fix the fire base not working

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,22 @@
 import React from 'react';
-<<<<<<< HEAD
 import Login from './LoginPage/Login';
 
 import './App.css';
-=======
-import firebase from './config/firebase';
->>>>>>> cc732d5a32249438acd042065643d9add9f0ab97
+// import firebase from './config/firebase';
 
 const App = () => {
     const handleClick = () => {
     }
     return (
-<<<<<<< HEAD
 
 
         <Login />
-=======
-        <div>
-            <h1>
-                {firebase.appId}
-                <button onClick={handleClick}>Click Me!</button>
-            </h1>
-        </div>
->>>>>>> cc732d5a32249438acd042065643d9add9f0ab97
+        // <div>
+        //     <h1>
+        //         {firebase.appId}
+        //         <button onClick={handleClick}>Click Me!</button>
+        //     </h1>
+        // </div>
     );
 }
 
