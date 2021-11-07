9658d5f08a8e2c88ffcc92c931ed031e35ec5048
Amanda <amandastephanieliu@gmail.com>
2021-11-07 16:29:40 -0500

    added total applicants buttons

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import Button from './Buttons';
+import ApplicantButton from './ApplicantButtons'
 import './Dashboard.css'
 
 const Dashboard = () => {
     return (
         <div className='dashboard-page'>
-
             <div className='dashboard-buttons'>
             <Button text="Total Applicants" barColor="#10275B" count={61}/>
             <Button text="New Applicants" barColor="#F44250" count={23}/>
             <Button text="Interviewing" barColor="#FF8427" count={4}/>
             <Button text="Background Check" barColor="#FCBB45" count={34}/>
+            </div>
+
+            <div className='total-applicants-title'>
+                <h2>Total Applicants</h2>
+            </div>
+            <div className='total-applicants'>
+                <ApplicantButton name="Bobby Smith"/>
+                <ApplicantButton name="Bobby Smith"/>
+                <ApplicantButton name="Bobby Smith"/>
+                <ApplicantButton name="Alice Jones"/>
             </div>
             
         </div>
