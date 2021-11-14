1036c3fe40da2279659f24890ec1228226c77048
Siddharth Cherukupalli <cherukusid@gmail.com>
2021-11-14 16:02:20 -0500

    made basic form for user info

--- src/ProfilePage/ProfileContent/UserInfoTab/UserInfo.tsx
+++ src/ProfilePage/ProfileContent/UserInfoTab/UserInfo.tsx
@@ -1,15 +1,93 @@
-import React from 'react';
-import ProfileHeader from './ProfileHeader/ProfileHeader';
-
+import React, { Component } from 'react';
 import './UserInfo.css';
 
 
-const UserInfo = () => {
-  return (
-    <div>
-      UserInfo Tab
-    </div>
-  )
+class UserInfo extends Component {
+
+  constructor(props: any) { 
+    super(props)
+
+    this.state = { 
+      username: '',
+      education: '',
+      phoneNumber: '',
+      email: '',
+      address: ''
+    }
+  }
+
+  handleOccupationChange = (event: any) => { 
+    this.setState({ 
+      username: event.target.value
+    })
+  }
+
+  handleEducationChange = (event: any) => { 
+    this.setState({ 
+      education: event.target.value
+    })
+  }
+
+  handlePhoneNumChange = (event: any) => { 
+    this.setState({
+      phoneNumber: event.target.value
+    })
+  }
+
+  handleEmailChange = (event: any) => { 
+    this.setState({ 
+      email: event.target.value
+    })
+  }
+
+  handleAddressChange = (event: any) => { 
+    this.setState({
+      address: event.target.value
+    })
+  }
+
+
+
+  render() {
+    return (
+      <form>
+        <div>
+          <label>Occupation</label>
+          <input type ="text" 
+          value = { this.state.username} 
+          onChange={this.handleOccupationChange} 
+          />
+
+          <label>Highest Education Attainment</label>
+          <input type = "text"
+          value = { this.state.education}
+          onChange = {this.handleEducationChange}
+          />
+
+          <label>Phone</label>
+          <input type = "number"
+          value = { this.state.phoneNumber}
+          onChange = { this.handlePhoneNumChange}
+          />
+
+          <label>Email</label>
+          <input type = "email"
+          value = { this.state.email}
+          onChange = { this.handleEmailChange}
+          />
+
+          <label>Address</label>
+          <input type = "text"
+          value = { this.state.address} 
+          onChange = { this.handleAddressChange}
+          />
+        </div>
+
+      </form>
+
+    )
+  }
 }
+
 
 export default UserInfo;
