74fd6762bf51fdf777949f43e8ea7523b7f2afd5
Aayushi Roy <aroy2530@terpmail.umd.edu>
2021-12-05 17:04:51 -0500

    initial draft of interview page

--- src/ProfilePage/ProfileContent/InterviewTab/Interview.tsx
+++ src/ProfilePage/ProfileContent/InterviewTab/Interview.tsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Calendar from 'react-calendar';
+import './CustomCalendar.css';
+import './Interview.css';
 
 const Interview = () => {
+  const date = new Date()
+  const time = "8 AM - 9 AM"
   return (
-    <div>
-      Interview
+    <div className='interview-container'>
+      <div className='interview-scheduled'>
+        <h2>This applicant's interview is scheduled to be on {date.toDateString()} at _______________.</h2>
+      </div>
+
+      <div className='calendar-and-notes'>
+        <div className='calendar-container'>
+          <Calendar className="react-calendar"
+            value={date}
+            showNeighboringMonth={false}
+          />
+
+          <div className='interview-time'>
+            {time}
+            <p></p>
+            Applicant Interview
+          </div>
+        </div>
+
+        <textarea className='interview-notes' placeholder="Notes" />
+      </div>
+      
     </div>
+
+    
   )
 }
 
