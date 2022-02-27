5dc4a126c9b6702edde402f156981ce585eac4a6
Jason Cavanaugh <jascav418@gmail.com>
2022-02-27 15:26:14 -0500

    Added a trainee dir with landing page perspective. Messy code. Made progress bar component.

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import Login from './login/Login';
 import Profile from './profile/Profile';
 import History from './history/History';
 import ResetPassword from './login/ResetPassword/ResetPassword';
+import Trainee from "./trainee/Trainee";
 
 
 const App = () => {
@@ -22,6 +23,7 @@ const App = () => {
                     <Route path='/resetPassword' element={<ResetPassword/>}/>
                     <Route path='/' element={<RequireAuth children={<Dashboard/>} />}/>
                     <Route path='/settings' element={<RequireAuth children={<Settings/>} />}/>
+                    <Route path='/trainee/home' element={<RequireAuth children={<Trainee/>} />}/>
                     <Route path='/history' element={<RequireAuth children={<History/>} />}/>
                     <Route path='/:id' element={<RequireAuth children={<Profile/>}/>} />
                     <Route path="*" element={<div>404 NOT FOUND</div>}/>
