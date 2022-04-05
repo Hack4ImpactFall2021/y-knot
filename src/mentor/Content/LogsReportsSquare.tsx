bd17c0d6a7d74f3c0319711824a02b58c22e3237
Jason Cavanaugh <jascav418@gmail.com>
2022-04-04 22:32:09 -0400

    Revert "add logs and reports images"
    This reverts commit 30e4b3415e95591a6e4a5cb2dc138bf2f42e429c.

--- src/mentor/Content/LogsReportsSquare.tsx
+++ src/mentor/Content/LogsReportsSquare.tsx
@@ -1,14 +1,12 @@
 import "./LogsReportsSquare.css";
 type Props = {
-    icon: string
     title: string
     text: string
 }
-const LogsReportsSquare: React.FC<Props> = ({icon, title, text}) => {
+const LogsReportsSquare: React.FC<Props> = ({title, text}) => {
     return (
         <div className="logs-reports-square">
             <h3 className="logs-reports-title"> 
-               {icon}
                {title}
                {text}
             </h3>
