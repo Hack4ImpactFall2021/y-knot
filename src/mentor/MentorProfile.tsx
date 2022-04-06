bb34c44a2ae1787bb2f3f248bf3d7ce0a96fa080
Jason Cavanaugh <jascav418@gmail.com>
2022-04-06 09:02:01 -0400

    Refactored code. Added modal to mentee list.

--- src/mentor/MentorProfile.tsx
+++ src/mentor/MentorProfile.tsx
@@ -1,105 +1,150 @@
-import React, { useState, useEffect } from 'react';
-import { DocumentData, DocumentSnapshot } from 'firebase/firestore';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import { DocumentData, DocumentSnapshot } from "firebase/firestore";
+import { useNavigate, useParams } from "react-router-dom";
 
-import './MentorProfile.css';
-import Content from './Content/Content';
-import NetworkManager, { Endpoints } from '../network/NetworkManager';
-import { Applicant, JotformResponse } from '../utils/utils';
-import close from '../profile/assets/close.png';
+import NetworkManager, { Endpoints } from "../network/NetworkManager";
+import { Applicant, JotformResponse } from "../utils/utils";
+import close from "../profile/assets/close.png";
 
-export enum Tabs { MentorInfo = "Your Profile" , MenteeProfile = "Mentee Profile"};
+import MentorInfo from "./Tabs/MentorInfo/MentorInfo";
+import Mentee from "./Tabs/Mentee/Mentee";
+
+import "./MentorProfile.css";
+
+export enum Tabs { MentorInfo = "Your Profile" , MenteeProfile = "Mentee Profile" };
 
 const MentorProfile = () => {
-    const navigate = useNavigate();
-    const { id } = useParams();
-    const [applicant, setApplicant] = useState<Applicant | null>(null);
-    const [formData, setFormData] = useState<JotformResponse | null>(null);
-    const [email, setEmail] = useState<string>("");
-    const [applicantLogin, setApplicantLogin] = useState<[string, string]>(["", ""]);
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const [applicant, setApplicant] = useState<Applicant | null>(null);
+  const [formData, setFormData] = useState<JotformResponse | null>(null);
 
-    const [tab, setTab] = useState<string>(Tabs.MentorInfo);
+  const [tab, setTab] = useState<string>(Tabs.MentorInfo);
 
-    useEffect(() => {
-        getApplicant();
-        getApplicantForm();
-    }, []);
+  useEffect(() => {
+    getApplicant();
+    getApplicantForm();
+  }, []);
 
 
-    const getApplicant = async () => {
-        try {
-            let snap = await NetworkManager.makeRequest(Endpoints.GetApplicant, { submissionId: id });
-            snap = snap as DocumentSnapshot<DocumentData>
-            if (snap.exists()) {
-                const data = snap.data();
-                setApplicant({
-                    firstName: data.first_name,
-                    lastName: data.last_name,
-                    email: data.email,
-                    phoneNumber: data.phone_number,
-                    stage: data.stage,
-                    submissionId: data.submission_id,
-                    notes: data.note || '',
-                    createdAt: data.createdAt
-                });
-                console.log(applicant);
-                setEmail(data.email);
-
-            } else {
-                throw new Error("not-found")
-            }
-        } catch (error) {
-            console.log(error);
-        }
+  const getApplicant = async () => {
+    try {
+      let snap = await NetworkManager.makeRequest(Endpoints.GetApplicant, { submissionId: id });
+      snap = snap as DocumentSnapshot<DocumentData>
+      if (!snap.exists()) {
+          throw new Error("not-found")
+      }
+      const data = snap.data();
+      setApplicant({
+        firstName: data.first_name,
+        lastName: data.last_name,
+        email: data.email,
+        phoneNumber: data.phone_number,
+        stage: data.stage,
+        submissionId: data.submission_id,
+        notes: data.note || "",
+        createdAt: data.createdAt
+      });
+      console.log(applicant);
+    } catch (error) {
+        console.log(error);
     }
+  }
 
-    const getApplicantForm = async () => {
-        try {
-            let data = await NetworkManager.makeRequest(Endpoints.GetApplicantForm, { id: id });
-            setFormData(data as JotformResponse);
-        } catch (error) {
-            console.log(error);
-        }
+  const getApplicantForm = async () => {
+    try {
+      let data = await NetworkManager.makeRequest(Endpoints.GetApplicantForm, { id: id });
+      setFormData(data as JotformResponse);
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-
-    if (!applicant) {
-        return (<div></div>);
-    }
-    return (
-        <div className="mentor-profile">
-            {/* Close button */}
-            <img 
-                className="exit-btn" 
-                src={close} 
-                onClick={() => navigate(-1)} 
-            />
-            <div className="mentor-profile-container">
-                {/* Mentor Name */}
-                <h1 className="mentor-name">
-                    {applicant.firstName} {applicant.lastName}
-                </h1>
-                {/* Tabs */}
-                <div className="mentor-profile-tabs">
-                    {Object.values(Tabs).map(curr => {
-                        return (
-                            <h1 
-                                key={curr} 
-                                className={curr === tab ? "tab-title selected" : "tab-title"} 
-                                onClick={e => setTab(e.currentTarget.innerHTML)}
-                            >
-                                {curr}
-                            </h1>
-                        );
-                    })}
-                </div>
-                <Content 
-                    type={tab} 
-                    formData={formData} 
-                />
-            </div>
-        </div>
+  /* RENDER FUNCTIONS */
+  const renderTabs = (tab: string) => {
+    const tabs = Object.values(Tabs).map(curr => 
+      <h1 key={curr} 
+        className={curr === tab ? "tab-title selected" : "tab-title"} 
+        onClick={e => setTab(e.currentTarget.innerHTML)}
+      >
+        {curr}
+      </h1>
     );
+    return tabs;
+  }
+
+  const getTabContents = (tab: string, formData: JotformResponse | null, menteeList: { name: string }[]) => {
+    switch (tab) {
+      case Tabs.MentorInfo:
+        return (
+          formData && <MentorInfo data={formData} />
+        );
+      case Tabs.MenteeProfile:
+        return (
+          <Mentee mentees={menteeList} />
+        );
+      default:
+        return null;
+    }
+  }
+
+  const menteeList = [
+      {
+          name: "Jason Cavanaugh"
+      },
+      {
+          name: "Amanda Liu"
+      },
+      {
+          name: "Jason Cavanaugh"
+      },
+      {
+          name: "Amanda Liu"
+      },
+      {
+          name: "Jason Cavanaugh"
+      },
+      {
+          name: "Amanda Liu"
+      },
+      {
+          name: "Jason Cavanaugh"
+      },
+      {
+          name: "Amanda Liu"
+      },
+      {
+          name: "Jason Cavanaugh"
+      },
+      {
+          name: "Amanda Liu"
+      },
+  ];
+
+  if (!applicant) {
+    return (<div></div>);
+  }
+  return (
+    <div className="mentor-profile">
+      {/* Close button */}
+      <img className="exit-btn" src={close} onClick={() => navigate(-1)} />
+
+      <div className="mentor-profile-container">
+        {/* Mentor Name */}
+        <h1 className="mentor-name">
+          {applicant.firstName} {applicant.lastName}
+        </h1>
+        {/* Tabs */}
+        <div className="mentor-profile-tabs">
+          {renderTabs(tab)}
+        </div>
+        {/* Tab Content */}
+        <section className="tab-content-wrapper">
+          {getTabContents(tab, formData, menteeList)}
+        </section>
+      </div>
+    </div>
+  );
 }
 
 export default MentorProfile;
---
+++ src/mentor/Tabs/MentorInfo/MentorInfo.tsx
@@ -0,0 +1,97 @@
+import React from "react";
+
+import "./MentorInfo.css";
+import { JotformResponse } from "../../../utils/utils";
+
+type Props = {
+  data: JotformResponse;
+}
+
+const MentorProfileForm : React.FC<Props> = ({ data }) => {
+
+  const response = data["content"]["answers"];
+
+  return (
+      <section className="mentor-profile-form">
+
+        <div className="occupation response-box">
+          <h1 className="response-title">Occupation</h1>
+          <div className="response-body">
+            <p className="response-text">{response["4"]["answer"]}</p>
+          </div>
+        </div>
+
+        <div className="education response-box">
+          <h1 className="response-title">Highest Educational Achivement</h1>
+          <div className="response-body">
+            <p className="response-text">{response["5"]["answer"]}</p>
+          </div>
+        </div>
+
+        <div className="phone response-box">
+            <h1 className="response-title">Phone Number</h1>
+            <div className="response-body">
+                <p className="response-text">{response["6"]["answer"] && response["6"]["prettyFormat"]}</p>
+            </div>
+        </div>
+
+        <div className="email response-box">
+          <h1 className="response-title">Email</h1>
+          <div className="response-body">
+            <p className="response-text">{response["7"]["answer"]}</p>
+          </div>
+        </div>
+
+        <div className="address response-box">
+          <h1 className="response-title address-title">Address</h1>
+
+          <div className="address1">
+            <div className="response-body">
+              <p className="response-text">{response["8"]["answer"] && response["8"]["answer"]["addr_line1"]}</p>
+              <p className="response-subtext">Street Address 1</p>
+            </div>
+          </div>
+
+          <div className="address2">
+            <div className="response-body">
+              <p className="response-text">{response["8"]["answer"] && response["8"]["answer"]["addr_line2"]}</p>
+              <p className="response-subtext">Street Address 2</p>
+            </div>
+          </div>
+
+          <div className="city">
+            <div className="response-body">
+              <p className="response-text">{response["8"]["answer"] && response["8"]["answer"]["city"]}</p>
+              <p className="response-subtext">City</p>
+            </div>
+          </div>
+
+          <div className="state">
+            <div className="response-body">
+              <p className="response-text">{response["8"]["answer"] && response["8"]["answer"]["state"]}</p>
+              <p className="response-subtext">State</p>
+            </div>
+          </div>
+
+          <div className="zip-code">
+            <div className="response-body">
+              <p className="response-text">{response["8"]["answer"] && response["8"]["answer"]["postal"]}</p>
+              <p className="response-subtext">Zip Code</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="background-check response-box">
+          <h1 className="response-title">Is user an employee of any of the following, FBI, Secret Service, CIA or police department?</h1>
+          <div className="response-body">
+            <input disabled type="radio" checked={response["38"]["answer"] === "Yes"} />
+            <p className="response-text radio-text">Yes</p>
+            <input disabled type="radio" checked={response["38"]["answer"] === "No"} />
+            <p className="response-text radio-text">No</p>
+          </div>
+        </div>
+      </section>
+  )
+}
+
+export default MentorProfileForm;
